54c3e115b99f93263c6a0773dd1a9c3ee4ec79ac
aayushker <singhaayushker@gmail.com>
2024-09-21 16:24:29 +0530

    Created the Routes

---
+++ frontend/pages/decode/index.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/components/Footer";
+import Decode from "@/components/Decode";
+import NavBar from "@/components/NavBar";
+import "@/app/globals.css";
+
+const index = () => {
+  return (
+    <div className="min-h-screen flex flex-col relative">
+      <header className="absolute top-0 left-0 w-full z-10">
+        <NavBar activePage="/decode" />
+      </header>
+      <main className="flex-grow h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center pt-16">
+        <Decode />
+      </main>
+      <footer className="flex-shrink-0 flex items-center justify-center">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default index;
---
+++ frontend/pages/encode/index.tsx
@@ -0,0 +1,23 @@
+import Footer from "@/components/Footer";
+import Encode from "@/components/Encode";
+import NavBar from "@/components/NavBar";
+import "@/app/globals.css";
+
+const index = () => {
+  return (
+    <div className="min-h-screen flex flex-col relative">
+      <header className="absolute top-0 left-0 w-full z-10">
+        <NavBar activePage="/encode" />
+      </header>
+      <main className="flex-grow h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center pt-16">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <Encode />
+      </main>
+      <footer className="flex-shrink-0 flex items-center justify-center">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default index;
