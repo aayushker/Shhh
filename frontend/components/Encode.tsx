c0459698b794390b89c04da1fd6e0333691d72ee
aayushker <singhaayushker@gmail.com>
2024-09-23 23:03:31 +0530

    Updated the Encode component

--- frontend/components/Encode.tsx
+++ frontend/components/Encode.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Card, Image, Button, Input, Divider } from "@nextui-org/react";
+import { Card, Image, Button, Input, Divider, CardHeader, Textarea } from "@nextui-org/react";
 import axios from "axios";
 
 const Encode = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full">
-      <Card fullWidth isBlurred className="mx-auto p-6 my-12 max-w-lg">
-        <div className="flex flex-col items-center">
-          <Divider className="w-full mb-4" />
+    <Card fullWidth isBlurred className=" max-w-screen-xl mx-auto p-6 my-12">
+        <div className="flex flex-col lg:flex-row items-center lg:items-start">
+          <Divider orientation="vertical" className="hidden lg:block mx-4" />
 
-          <div className="w-full flex justify-center p-4">
-            <Image src="/images/contact.png" height={200} />
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end p-4">
+            <Image src="/images/encrypted.gif" height={400} />
           </div>
-          <div className="w-full max-w-md p-4">
+          <div className="w-full lg:w-1/2 max-w-md p-4">
             <Input
               fullWidth
               size="lg"
-              type="text"
+              type="image"
               label="Name"
-              placeholder="Enter your Name"
+              placeholder="Add the image"
               className="mb-4"
+              // value={formData.name}
+              // onChange={handleChange}
             />
             <Input
               fullWidth
@@ -28,32 +29,46 @@ const Encode = () => {
               label="Email"
               placeholder="Enter your Email"
               className="mb-4"
-            />
-            <Input
-              fullWidth
-              size="lg"
-              type="file"
-              label="Upload Image"
-              className="mb-4"
+              // value={formData.email}
+              // onChange={handleChange}
             />
             <Input
               fullWidth
               size="lg"
               type="text"
-              label="Text"
-              placeholder="Enter your Text"
+              label="Subject"
+              placeholder="Enter your Subject"
               className="mb-4"
+              // value={formData.subject}
+              // onChange={handleChange}
             />
-            <div className="flex justify-center items-center">
-              <Button className="mt-4 hover:bg-green-400">
-                Submit
+            <Textarea
+              fullWidth
+              size="lg"
+              type="text"
+              label="Message"
+              placeholder="Enter your Feedback"
+              className="mb-4"
+              rows={4}
+              // value={formData.message}
+              // onChange={handleChange}
+            />
+            <div className="flex justify-center align-middle">
+              <Button
+                className="mt-4 hover:bg-green-400"
+                type="submit"
+                // onClick={handleSubmit}
+                // onSubmit={handleSubmit}
+                // disabled={loading}
+              >
+                {/* {loading ? <Spinner /> : "Submit"} */}
               </Button>
             </div>
           </div>
         </div>
-        <Divider className="w-full mt-4" />
+
+        <Divider className="block lg:hidden my-4" />
       </Card>
-    </div>
   );
 };
 
