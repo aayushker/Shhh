58fa082d0f4e1001a92c2bc4c5a3670e4f3a665f
aayushker <singhaayushker@gmail.com>
2024-10-02 12:41:34 +0530

    Updated the index pages

--- frontend/pages/learn/index.tsx
+++ frontend/pages/learn/index.tsx
@@ -10,6 +10,7 @@ const index = () => {
         <NavBar activePage="/learn" />
       </header>
       <main className="flex-grow h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center pt-16">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Learn />
       </main>
       <footer className="flex-shrink-0 flex items-center justify-center">
