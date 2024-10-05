2c02ce6356e0ece52564d6f0780f1e3fe54d1775
aayushker <singhaayushker@gmail.com>
2024-10-05 15:24:03 +0530

    Added support for other image formats

--- backend/encode/views.py
+++ backend/encode/views.py
@@ -1,5 +1,5 @@
 # from .serializers import EncodeSerializers
-from .Process import encode, decode
+from .Process import encode, decode, Convert
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import FileResponse
@@ -17,27 +17,32 @@ class Encode(APIView):
         image_file = request.FILES.get('image')
         text = request.data.get('text')
 
-        # Check if the image and text is supplied or not
+        # Check if the image and text are supplied
         if not image_file or not text:
             return Response({"error": "Image and text are required."}, status=400)
         
         image = Image.open(image_file)
         
-         # Check if the image is in a lossless format
+        # Check if the image is in a lossless format
         if image.format.lower() not in ['png', 'bmp', 'tiff']:
-            return Response({"error": "Please use a lossless image format like PNG or BMP."}, status=400)
+            # Convert lossy image format to PNG
+            image = Convert(image)
+            if isinstance(image, str):  # Check if Convert function returned an error message
+                return Response({"error": image}, status=400)
         
-        # encode 
+        # Encode the image with the message
         encoded_image = encode(image, text)
 
+        # Save the encoded image as PNG
         image_io = io.BytesIO()
-        encoded_image.save(image_io, format=image.format)
+        encoded_image.save(image_io, format='PNG')  # Always save as PNG
         image_io.seek(0)
 
-        response = FileResponse(image_io, content_type=f'image/{image.format.lower()}')
-        response['Content-Disposition'] = f'attachment; filename="processed_image.{image.format.lower()}"'
+        # Return the processed image as a downloadable file
+        response = FileResponse(image_io, content_type='image/png')
+        response['Content-Disposition'] = 'attachment; filename="processed_image.png"'
         return response
-    
+
 class Decode(APIView):
     def post(self, request, *args, **kwargs):
         
