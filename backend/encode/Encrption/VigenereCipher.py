0a9b2cbc085829ecf6fe5326bf078db2d25a66b0
aayushker <singhaayushker@gmail.com>
2024-10-13 18:46:55 +0530

    Updated VigenereCipher for production

--- backend/encode/Encrption/VigenereCipher.py
+++ backend/encode/Encrption/VigenereCipher.py
@@ -1,4 +1,7 @@
-def vigenere_encrypt(text, keyword):
+def encrypt_VigenereCipher(text, keyword):
+    if not keyword.isalpha():
+        raise ValueError("Keyword must contain only alphabetic characters")
+    
     result = []
     keyword_repeat = (keyword * (len(text) // len(keyword))) + keyword[:len(text) % len(keyword)]
     
@@ -6,13 +9,19 @@ def vigenere_encrypt(text, keyword):
         letter = text[i]
         if letter.isalpha():
             shift = ord(keyword_repeat[i].lower()) - 97
-            result.append(chr((ord(letter.lower()) + shift - 97) % 26 + 97))
+            if letter.isupper():
+                result.append(chr((ord(letter) + shift - 65) % 26 + 65))
+            else:
+                result.append(chr((ord(letter) + shift - 97) % 26 + 97))
         else:
             result.append(letter)
     
     return ''.join(result)
 
-def vigenere_decrypt(cipher_text, keyword):
+def decrypt_VigenereCipher(cipher_text, keyword):
+    if not keyword.isalpha():
+        raise ValueError("Keyword must contain only alphabetic characters")
+    
     result = []
     keyword_repeat = (keyword * (len(cipher_text) // len(keyword))) + keyword[:len(cipher_text) % len(keyword)]
     
@@ -20,18 +29,11 @@ def vigenere_decrypt(cipher_text, keyword):
         letter = cipher_text[i]
         if letter.isalpha():
             shift = ord(keyword_repeat[i].lower()) - 97
-            result.append(chr((ord(letter.lower()) - shift - 97) % 26 + 97))
+            if letter.isupper():
+                result.append(chr((ord(letter) - shift - 65) % 26 + 65))
+            else:
+                result.append(chr((ord(letter) - shift - 97) % 26 + 97))
         else:
             result.append(letter)
     
     return ''.join(result)
-
-# Example usage
-message = "This is a secret message!"
-keyword = "key"
-
-encrypted = vigenere_encrypt(message, keyword)
-print("Encrypted:", encrypted)
-
-decrypted = vigenere_decrypt(encrypted, keyword)
-print("Decrypted:", decrypted)
