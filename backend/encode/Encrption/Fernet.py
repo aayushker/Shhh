d03c5af44a143225d65f62308de9829430fa9ed5
aayushker <singhaayushker@gmail.com>
2024-10-13 18:40:35 +0530

    Updated Fernet for production

--- backend/encode/Encrption/Fernet.py
+++ backend/encode/Encrption/Fernet.py
@@ -1,14 +1,19 @@
 from cryptography.fernet import Fernet
 
-# Generate a key
-key = Fernet.generate_key()
-cipher = Fernet(key)
-
 # Encrypt
-message = "This is a secret message!"
-encrypted = cipher.encrypt(message.encode())
-print("Encrypted:", encrypted)
+def encrypt_Fernet(message):
+    # Generate a key
+    key = Fernet.generate_key()
+    cipher = Fernet(key)
+    
+    encrypted = cipher.encrypt(message.encode())
+    return encrypted, key
 
 # Decrypt
-decrypted = cipher.decrypt(encrypted).decode()
-print("Decrypted:", decrypted)
+def decrypt_Fernet(message, key):
+    try:
+        cipher = Fernet(key)
+        decrypted = cipher.decrypt(message).decode()
+        return decrypted
+    except Exception as e:
+        return f'Error occurred: {e}'
