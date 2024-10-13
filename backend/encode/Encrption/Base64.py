53500aa3dd877225145a8870209c01830030b851
aayushker <singhaayushker@gmail.com>
2024-10-13 17:37:07 +0530

    Updated Base64 for further processing

--- backend/encode/Encrption/Base64.py
+++ backend/encode/Encrption/Base64.py
@@ -1,10 +1,14 @@
 import base64
 
 # Encode
-message = "This is a secret message!"
-encoded = base64.b64encode(message.encode('utf-8')).decode('utf-8')
-print("Encoded:", encoded)
+def encrypt_Base64(message):
+    encoded = base64.b64encode(message.encode('utf-8')).decode('utf-8')
+    return encoded
 
 # Decode
-decoded = base64.b64decode(encoded).decode('utf-8')
-print("Decoded:", decoded)
+def decrypt_Base64(message):
+    try:
+        decoded = base64.b64decode(message).decode('utf-8')
+        return decoded
+    except (base64.binascii.Error, UnicodeDecodeError) as e:
+        return f'Error occurred: {e}'
