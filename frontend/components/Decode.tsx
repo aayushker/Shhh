73d87093c80b7fbe90715a881c64424793cfe8eb
aayushker <singhaayushker@gmail.com>
2024-10-15 23:01:45 +0530

    Updated Decode component

--- frontend/components/Decode.tsx
+++ frontend/components/Decode.tsx
@@ -52,11 +52,7 @@ const Decode = () => {
     formData.append("image", blob);
 
     try {
-      const response = await axios.post(
-        `${url}/api/decode/`,
-        formData
-      );
-
+      const response = await axios.post(`${url}/api/decode/`, formData);
       const mssg = response.data;
       setMessage(mssg);
       setConfirmation("Image processed successfully!");
@@ -66,17 +62,20 @@ const Decode = () => {
   };
 
   return (
-    <div>
-      <p className="text-2xl sm:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+    <div className="px-4 py-6 sm:px-6 md:px-12 lg:px-20">
+      <p className="text-2xl sm:text-4xl md:text-5xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4">
         Add your <strong className="text-red-400">image</strong> to{" "}
         <strong className="text-red-400">decode</strong> the hidden{" "}
         <strong className="bg-gradient-to-r from-stone-500 to-stone-700 bg-clip-text text-transparent">
           message
         </strong>
       </p>
-      <Card isBlurred className="max-w-lg w-full mx-auto p-6 my-12">
+      <Card
+        isBlurred
+        className="max-w-md w-full mx-auto p-4 sm:p-6 my-8 md:my-12 bg-opacity-50"
+      >
         <div
-          className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-4 cursor-pointer"
+          className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-4 cursor-pointer h-64 md:h-80 w-full"
           onClick={handleClick}
           onDrop={handleDrop}
           onDragOver={handleDragOver}
@@ -85,11 +84,11 @@ const Decode = () => {
             <Image
               src={image as string}
               alt="Uploaded Image"
-              className="w-full h-auto max-h-24"
+              className="w-fit h-fit max-h-full"
             />
           ) : (
             <div className="text-center">
-              <p className="text-gray-500">
+              <p className="text-gray-500 text-sm md:text-lg">
                 Click to upload or drag and drop an image
               </p>
             </div>
@@ -102,13 +101,22 @@ const Decode = () => {
           />
         </div>
         <Divider className="my-4" />
-        <Button className="mt-4 hover:bg-green-400" onClick={handleSubmit}>
+        <Button className="mt-4 w-full bg-gradient-to-r from-green-400 to-blue-500 hover:bg-gradient-to-l" onClick={handleSubmit}>
           Submit
         </Button>
-        {confirmation && <p className="mt-4 text-center">{confirmation}</p>}
+        {confirmation && (
+          <p className="mt-4 text-center text-sm md:text-base">
+            {confirmation}
+          </p>
+        )}
         {message && (
           <div className="mt-4 text-center">
-            <Textarea readOnly value={message} label="The hidden message is- "/>
+            <Textarea
+              readOnly
+              value={message}
+              label="The hidden message is- "
+              className="text-sm md:text-base"
+            />
           </div>
         )}
       </Card>
