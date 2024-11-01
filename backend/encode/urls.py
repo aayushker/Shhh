f9f8f055a6aa3508dc8873ca0c5fb005da6c246a
aayushker <singhaayushker@gmail.com>
2024-11-01 23:11:47 +0530

    Updated URLS for test

--- backend/encode/urls.py
+++ backend/encode/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Encode, Decode
+from .views import Encode, Decode, testEncode, testDecode
 
 urlpatterns = [
     path('encode/', Encode.as_view(), name='encode'),
     path('decode/', Decode.as_view(), name='decode'),
+    path('testencode/', testEncode.as_view(), name='testencode'),
+    path('testdecode/', testDecode.as_view(), name='testdecode'),
 ]
